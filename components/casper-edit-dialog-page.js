8092b067b3c7b20725b1a8b89fd0d426d588e7f6
Rita Silva <arita.lcas@gmail.com>
2023-04-13 16:25:12 +0100

    Added missing methods

--- components/casper-edit-dialog-page.js
+++ components/casper-edit-dialog-page.js
@@ -1,13 +1,55 @@
-import { LitElement } from 'lit';
+import { LitElement, css } from 'lit';
 
 
 export class CasperEditDialogPage extends LitElement {
+  static styles = css`
+    :host {
+      display: grid;
+      column-gap: 12px;
+      row-gap: 6px;
+    }
+
+    h3 {
+      color: var(--primary-color);
+    }
+  `;
+
   validate () {
-    throw 'Error! A validate method must be defined for the page.';
+    console.warn('A validate method should be defined for the page.')
+    return true;
   }
 
-  save () {
-    throw 'Error! A save method must be defined for the page.';
+  load (data) {
+    for (const elem of this.shadowRoot.querySelectorAll('[binding]')) {
+      const binding = elem.getAttribute('binding');
+
+      console.log(elem.tagName.toLowerCase());
+      console.log(`binding to ${binding} = ${data[binding]} type: ${typeof data[binding]}`);
+      switch (typeof data[binding]) {
+        case 'boolean':
+          elem.checked = data[binding];
+          break;
+        default:
+          elem.value = data[binding];
+          break;
+      }
+    }
+  }
+
+  save (patch, data) {
+    for (const elem of this.shadowRoot.querySelectorAll('[binding]')) {
+      const binding = elem.getAttribute('binding')
+      if ( elem.value != data[binding] ) {
+        switch (typeof data[binding]) {
+          case 'boolean':
+            patch.set(binding, elem.checked);
+            break;
+          default:
+            patch.set(binding, elem.value);
+            break;
+        }
+      }
+    }
   }
 
   showStatusPage (response) {
