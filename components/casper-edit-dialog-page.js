250f9dc8fb546567a94aa59dc88f203f971a7e19
Rita Silva <arita.lcas@gmail.com>
2023-06-30 16:00:18 +0100

    Added option to run jobs in the background

--- components/casper-edit-dialog-page.js
+++ components/casper-edit-dialog-page.js
@@ -280,8 +280,8 @@ export class CasperEditDialogPage extends LitElement {
     this.afterSave(saveData, data);
   }
 
-  showStatusPage (response) {
-    this.editDialog.showStatusPage(response);
+  showStatusPage (response, status) {
+    this.editDialog.showStatusPage(response, status);
   }
 
   hideStatusAndProgress () {
